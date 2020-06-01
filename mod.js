11238cf481d123f2005a50ea8fe6c9c8195b197b
Sany <44934721+often@users.noreply.github.com>
2020-06-01 04:51:26 -0600

    Update mod.js
    switched from a class to function because classes aren't needed for such simple thing

--- mod.js
+++ mod.js
@@ -1,33 +1,31 @@
-export default class proxyScrape {
-	static async get(type, timeout, countryCode, ssl, anonymity) {
-		if (!type) throw 'missing proxy type (http/socks4/socks5)'
+export default async function proxyScrape(type, timeout, countryCode, ssl, anonymity) {
+	if (!type) throw 'missing proxy type (http/socks4/socks5)'
 
-		const types = ['http', 'socks4', 'socks5']
+	const types = ['http', 'socks4', 'socks5']
 
-		if (!types.includes(type)) throw 'invalid proxy type, available ones are: http, socks4 and socks5'
+	if (!types.includes(type)) throw 'invalid proxy type, available ones are: http, socks4 and socks5'
 
-		if (!timeout) throw 'missing proxy timeout (ms)'
+	if (!timeout) throw 'missing proxy timeout (ms)'
 
-		if (!Number.isInteger(timeout)) throw 'timeout must be an integer'
+	if (!Number.isInteger(timeout)) throw 'timeout must be an integer'
 
-		if (timeout < 50 || timeout > 10000) throw 'timeout must be in range between 50 and 10000ms'
+	if (timeout < 50 || timeout > 10000) throw 'timeout must be in range between 50 and 10000ms'
 
-		if (!countryCode) throw 'missing country code, for more see: https://countrycode.org/'
+	if (!countryCode) throw 'missing country code, for more see: https://countrycode.org/'
 
-		if (!ssl) throw 'missing SSL support type (all/yes/no)'
+	if (!ssl) throw 'missing SSL support type (all/yes/no)'
 
-		const ssl_types = ['all', 'yes', 'no']
+	const ssl_types = ['all', 'yes', 'no']
 
-		if (!ssl_types.includes(ssl)) throw 'invalid SSL support type, available ones are: all, yes and no'
+	if (!ssl_types.includes(ssl)) throw 'invalid SSL support type, available ones are: all, yes and no'
 
-		if (!anonymity) throw 'missing anonymity type (all/elite/anonymous/transparent)'
+	if (!anonymity) throw 'missing anonymity type (all/elite/anonymous/transparent)'
 
-		const anonymity_types = ['all', 'elite', 'anonymous', 'transparent']
+	const anonymity_types = ['all', 'elite', 'anonymous', 'transparent']
 
-		if (!anonymity_types.includes(anonymity)) throw 'invalid anonymity type, available ones are: all, elite, anonymous, transparent'
+	if (!anonymity_types.includes(anonymity)) throw 'invalid anonymity type, available ones are: all, elite, anonymous, transparent'
 
-		const scraped = await fetch('https://api.proxyscrape.com/?request=displayproxies&proxytype=' + type + '&timeout=' + timeout + '&country=' + countryCode + '&ssl=' + ssl + '&anonymity=' + anonymity)
+	const scraped = await fetch('https://api.proxyscrape.com/?request=displayproxies&proxytype=' + type + '&timeout=' + timeout + '&country=' + countryCode + '&ssl=' + ssl + '&anonymity=' + anonymity)
 
-		return scraped.text()
-	}
+	return scraped.text()
 }
