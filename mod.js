9bab8b5f6395363fc3ec8a0401ea97ba64c2bad7
Sany <44934721+often@users.noreply.github.com>
2020-06-01 06:20:46 -0600

    ¯\_(ツ)_/¯

--- mod.js
+++ mod.js
@@ -1,11 +1,11 @@
 export default async function proxyScrape(type, timeout, countryCode, ssl, anonymity) {
-	if (!type) throw 'missing proxy type (http/socks4/socks5)'
+	if (!type) throw 'missing proxy type'
 
 	const types = ['http', 'socks4', 'socks5']
 
-	if (!types.includes(type)) throw 'invalid proxy type, available ones are: http, socks4 and socks5'
+	if (!types.includes(type)) throw 'invalid proxy type, available ones are: ' + types.join(', ')
 
-	if (!timeout) throw 'missing proxy timeout (ms)'
+	if (!timeout) throw 'missing proxy timeout (in ms)'
 
 	if (!Number.isInteger(timeout)) throw 'timeout must be an integer'
 
@@ -13,17 +13,17 @@ export default async function proxyScrape(type, timeout, countryCode, ssl, anony
 
 	if (!countryCode) throw 'missing country code, for more see: https://countrycode.org/'
 
-	if (!ssl) throw 'missing SSL support type (all/yes/no)'
+	if (!ssl) throw 'missing SSL support type'
 
 	const ssl_types = ['all', 'yes', 'no']
 
-	if (!ssl_types.includes(ssl)) throw 'invalid SSL support type, available ones are: all, yes and no'
+	if (!ssl_types.includes(ssl)) throw 'invalid SSL support type, available ones are: ' + ssl_types.join(', ')
 
-	if (!anonymity) throw 'missing anonymity type (all/elite/anonymous/transparent)'
+	if (!anonymity) throw 'missing anonymity type'
 
 	const anonymity_types = ['all', 'elite', 'anonymous', 'transparent']
 
-	if (!anonymity_types.includes(anonymity)) throw 'invalid anonymity type, available ones are: all, elite, anonymous, transparent'
+	if (!anonymity_types.includes(anonymity)) throw 'invalid anonymity type, available ones are: ' + anonymity_types.join(', ')
 
 	const scraped = await fetch('https://api.proxyscrape.com/?request=displayproxies&proxytype=' + type + '&timeout=' + timeout + '&country=' + countryCode + '&ssl=' + ssl + '&anonymity=' + anonymity)
 
